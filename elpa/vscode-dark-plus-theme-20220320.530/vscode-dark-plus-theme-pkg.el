cf9ed3d8a273ef87176b15f78a54a3bf88e1deb0
trapano-monogamo <filippo.cozzi.fc@gmail.com>
2022-07-29 10:13:58 +0200

    .

--- elpa/vscode-dark-plus-theme-20220320.530/vscode-dark-plus-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from vscode-dark-plus-theme.el  -*- no-byte-compile: t -*-
-(define-package "vscode-dark-plus-theme" "20220320.530" "Default Visual Studio Code Dark+ theme" 'nil :commit "d247fcf2b0c4f21398276e12faf3f2a4c26b2306" :authors '(("Ian Y.E. Pan")) :maintainer '("Ian Y.E. Pan") :url "https://github.com/ianpan870102/vscode-dark-plus-emacs-theme")
